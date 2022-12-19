21cd2d62e16e4559edcc5d684072199b2160f8a7
Hossam Elshamy <hossamelshamyy@gmail.com>
2022-12-19 15:13:51 +0200

    part1 fininshed

--- client.py
+++ client.py
@@ -3,31 +3,31 @@ import socket
 # SERVER = input()
 # PORT = int(input())
 SERVER = "127.0.0.1"
-PORT = 6666
+PORT = 6665
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.settimeout(0.5)
 
 try:
 	client.connect((SERVER, PORT))
 	answer = client.recv(1024)
+	print("Example : 4 + 5")
+	print("Type 'Over' to terminate")
 except:
 	exit("Server busy")
 
 while True:
-	print("Example : 4 + 5")
-	inp = input("Enter the operation in the form opreand operator oprenad: ")
+	try:
+		inp = input("Enter the operation in the form opreand operator oprenad: ")
+	except:
+		exit("Client closed")
 	if inp == "Over":
 		client.sendall(inp.encode())
 		break
-	# try:
 	client.sendall(inp.encode())
-	# except:
-		# exit("Error")
-	# try:
+
 	answer = client.recv(1024)
-	# except:
-		# exit("Server busy")
+
 	print("Answer is "+answer.decode())
-	print("Type 'Over' to terminate")
+	
 
 client.close()
--- server1.py
+++ server1.py
@@ -1,7 +1,7 @@
 import socket
 
 LOCALHOST = "127.0.0.1"
-PORT = 6666
+PORT = 6669
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind((LOCALHOST, PORT))
 server.listen(1)
@@ -12,24 +12,16 @@ while True:
 	clientConnection, clientAddress = server.accept()
 	clientConnection.sendall('Connected'.encode())
 	while True: 
-		try: 
-			msg = ''
-			data = clientConnection.recv(1024)
-			msg = data.decode()
-			if msg == '' or msg == 'Over':
-				print("Connection is Over")
-				break
-			print("Equation is recievied")
-			result = eval(msg)
-			print("Send the result to client")
-			output = str(result)
-			clientConnection.sendall(output.encode())
-		except:
+		msg = ''
+		data = clientConnection.recv(1024)
+		msg = data.decode()
+		if msg == '' or msg == 'Over':
+			print("Connection is Over")
 			break
+		print("Equation is recievied")
+		result = eval(msg)
+		print("Send the result to client")
+		output = str(result)
+		clientConnection.sendall(output.encode())
 	clientConnection.close()
-	server.settimeout(0.5)
-	try:
-		clientConnection, clientAddress = server.accept()
-		clientConnection.sendall('Connected'.encode())
-	except:
-		exit("Server closed")
+	server.settimeout(100000)
