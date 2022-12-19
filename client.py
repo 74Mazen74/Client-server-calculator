fe52344c7e5a75344ee0e153b728d380d188fa6f
Hossam Elshamy <hossamelshamyy@gmail.com>
2022-12-19 14:14:21 +0200

    b

--- client.py
+++ client.py
@@ -1,22 +1,32 @@
 import socket
 
-
-SERVER = input()
-PORT = int(input())
+# SERVER = input()
+# PORT = int(input())
+SERVER = "127.0.0.1"
+PORT = 6666
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client.connect((SERVER, PORT))
-	
+client.settimeout(0.5)
+
+try:
+	client.connect((SERVER, PORT))
+	answer = client.recv(1024)
+except:
+	exit("Server busy")
+
 while True:
 	print("Example : 4 + 5")
 	inp = input("Enter the operation in the form opreand operator oprenad: ")
 	if inp == "Over":
-		client.send(inp.encode())
-		break
-	try:
 		client.sendall(inp.encode())
-	except:
-		exit("Error")
+		break
+	# try:
+	client.sendall(inp.encode())
+	# except:
+		# exit("Error")
+	# try:
 	answer = client.recv(1024)
+	# except:
+		# exit("Server busy")
 	print("Answer is "+answer.decode())
 	print("Type 'Over' to terminate")
 
--- server1.py
+++ server1.py
@@ -1,15 +1,16 @@
 import socket
 
 LOCALHOST = "127.0.0.1"
-PORT = 5050
+PORT = 6666
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind((LOCALHOST, PORT))
 server.listen(1)
+
 print("Server started")
 print("Waiting for client request..")
 while True:
 	clientConnection, clientAddress = server.accept()
-	print("Connected client :", clientAddress)
+	clientConnection.sendall('Connected'.encode())
 	while True: 
 		try: 
 			msg = ''
@@ -22,7 +23,13 @@ while True:
 			result = eval(msg)
 			print("Send the result to client")
 			output = str(result)
-			clientConnection.send(output.encode())
+			clientConnection.sendall(output.encode())
 		except:
 			break
 	clientConnection.close()
+	server.settimeout(0.5)
+	try:
+		clientConnection, clientAddress = server.accept()
+		clientConnection.sendall('Connected'.encode())
+	except:
+		exit("Server closed")
